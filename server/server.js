6f4eb9c41e686da19c2cce2a9d3c0c9fd764be90
ruslanV <ruslan.vasylenko87@gmail.com>
2017-02-24 18:55:13 +0200

    Init commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos })
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((doc) => {
+    if (!doc) return res.status(404).send();
+    res.send({ doc });
+  }).catch((e) => res.status(400).send(e));
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
